fe34e7d242160bec904303afb659a965e947c90f
virtualdiamondtoken <84251860+virtualdiamondtoken@users.noreply.github.com>
2021-05-16 00:18:30 +0200

    1

---
+++ precache-manifest.301b0e5bb4c31d1792a59ece09921ac7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "846b2edd7aba5e317791e729c2230350",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51585005c4ab0c369c28",
+    "url": "/static/css/main.e4f782f6.chunk.css"
+  },
+  {
+    "revision": "1d4db5fb5dad98429157",
+    "url": "/static/js/2.10238e96.chunk.js"
+  },
+  {
+    "revision": "51585005c4ab0c369c28",
+    "url": "/static/js/main.a0ec29c3.chunk.js"
+  },
+  {
+    "revision": "80b789232f75104194ee",
+    "url": "/static/js/runtime-main.e2fb13b6.js"
+  }
+]);
