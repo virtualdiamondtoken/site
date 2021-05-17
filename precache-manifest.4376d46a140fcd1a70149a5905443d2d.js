cf34ff9149bdad354d9fa7fffc049f8e702e86a6
virtualdiamondtoken <84251860+virtualdiamondtoken@users.noreply.github.com>
2021-05-17 14:13:32 +0200

    1

---
+++ precache-manifest.4376d46a140fcd1a70149a5905443d2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f52379884744421f08853107f1d97ebe",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "620246a2f96d3519c0db",
+    "url": "/site/static/css/main.e4f782f6.chunk.css"
+  },
+  {
+    "revision": "605fd9d53ee65f885acc",
+    "url": "/site/static/js/2.bb423696.chunk.js"
+  },
+  {
+    "revision": "620246a2f96d3519c0db",
+    "url": "/site/static/js/main.8156ac80.chunk.js"
+  },
+  {
+    "revision": "d5734a3a772fa528728c",
+    "url": "/site/static/js/runtime-main.535bf4bc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/site/precache-manifest.87e60b56f737ca1f456c214bb77ec5fa.js"
+  "/site/precache-manifest.4376d46a140fcd1a70149a5905443d2d.js"
 );
 
 self.addEventListener('message', (event) => {
